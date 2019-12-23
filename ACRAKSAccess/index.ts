3643d3ff9c1d9583c96f45ea4bc998c2f9bf2e60
lturmel <lturmel@expertasolutions.com>
2019-12-23 08:54:51 -0500

    add try..catch
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- ACRAKSAccess/index.ts
+++ ACRAKSAccess/index.ts
@@ -10,134 +10,138 @@ async function LoginToAzure(servicePrincipalId:string, servicePrincipalKey:strin
 };
 
 async function run() {
-  let acrSubscriptionEndpoint = tl.getInput("acrSubscriptionEndpoint", true) as string;
-  let acrSubcriptionId = tl.getEndpointDataParameter(acrSubscriptionEndpoint, "subscriptionId", false) as string;
-  let acrServicePrincipalId = tl.getEndpointAuthorizationParameter(acrSubscriptionEndpoint, "serviceprincipalid", false) as string;
-  let acrServicePrincipalKey = tl.getEndpointAuthorizationParameter(acrSubscriptionEndpoint, "serviceprincipalkey", false) as string;
-  let acrTenantId = tl.getEndpointAuthorizationParameter(acrSubscriptionEndpoint,"tenantid", false) as string;
+  try { 
+    let acrSubscriptionEndpoint = tl.getInput("acrSubscriptionEndpoint", true) as string;
+    let acrSubcriptionId = tl.getEndpointDataParameter(acrSubscriptionEndpoint, "subscriptionId", false) as string;
+    let acrServicePrincipalId = tl.getEndpointAuthorizationParameter(acrSubscriptionEndpoint, "serviceprincipalid", false) as string;
+    let acrServicePrincipalKey = tl.getEndpointAuthorizationParameter(acrSubscriptionEndpoint, "serviceprincipalkey", false) as string;
+    let acrTenantId = tl.getEndpointAuthorizationParameter(acrSubscriptionEndpoint,"tenantid", false) as string;
 
-  let aksSubscriptionEndpoint = tl.getInput("aksSubscriptionEndpoint", true) as string;
-  let aksSubcriptionId = tl.getEndpointDataParameter(aksSubscriptionEndpoint, "subscriptionId", false) as string;
-  let aksServicePrincipalId = tl.getEndpointAuthorizationParameter(aksSubscriptionEndpoint, "serviceprincipalid", false) as string;
-  let aksServicePrincipalKey = tl.getEndpointAuthorizationParameter(aksSubscriptionEndpoint, "serviceprincipalkey", false) as string;
-  let aksTenantId = tl.getEndpointAuthorizationParameter(aksSubscriptionEndpoint,"tenantid", false) as string;
+    let aksSubscriptionEndpoint = tl.getInput("aksSubscriptionEndpoint", true) as string;
+    let aksSubcriptionId = tl.getEndpointDataParameter(aksSubscriptionEndpoint, "subscriptionId", false) as string;
+    let aksServicePrincipalId = tl.getEndpointAuthorizationParameter(aksSubscriptionEndpoint, "serviceprincipalid", false) as string;
+    let aksServicePrincipalKey = tl.getEndpointAuthorizationParameter(aksSubscriptionEndpoint, "serviceprincipalkey", false) as string;
+    let aksTenantId = tl.getEndpointAuthorizationParameter(aksSubscriptionEndpoint,"tenantid", false) as string;
 
-  let registerMode = tl.getInput("registerMode", true) as string;
-  let acrResourceGroup = tl.getInput("acrResourceGroupName", true) as string;
-  let containerRegistry = tl.getInput("containerRegistry", true) as string;
-  let aksResourceGroup = tl.getInput("aksResourceGroupName", true) as string;
-  let aksCluster = tl.getInput("aksCluster", true) as string;
-  let acrUsername = tl.getInput("acrUsername", false) as string;
-  let acrPassword = tl.getInput("acrPassword", false) as string;
+    let registerMode = tl.getInput("registerMode", true) as string;
+    let acrResourceGroup = tl.getInput("acrResourceGroupName", true) as string;
+    let containerRegistry = tl.getInput("containerRegistry", true) as string;
+    let aksResourceGroup = tl.getInput("aksResourceGroupName", true) as string;
+    let aksCluster = tl.getInput("aksCluster", true) as string;
+    let acrUsername = tl.getInput("acrUsername", false) as string;
+    let acrPassword = tl.getInput("acrPassword", false) as string;
 
-  /* ACR Subscription Informations */
-  console.log("ACR Azure Subscription Id: " + acrSubcriptionId);
-  console.log("ACR ServicePrincipalId: " + acrServicePrincipalId);
-  console.log("ACR ServicePrincipalKey: " + acrServicePrincipalKey);
-  console.log("ACR Tenant Id: " + acrTenantId);
-  /* End of ACR Subscription Informations */
+    /* ACR Subscription Informations */
+    console.log("ACR Azure Subscription Id: " + acrSubcriptionId);
+    console.log("ACR ServicePrincipalId: " + acrServicePrincipalId);
+    console.log("ACR ServicePrincipalKey: " + acrServicePrincipalKey);
+    console.log("ACR Tenant Id: " + acrTenantId);
+    /* End of ACR Subscription Informations */
 
-  console.log("");
-  /* AKS Subscription Informations */
-  console.log("AKS Azure Subscription Id: " + aksSubcriptionId);
-  console.log("AKS ServicePrincipalId: " + aksServicePrincipalId);
-  console.log("AKS ServicePrincipalKey: " + aksServicePrincipalKey);
-  console.log("AKS Tenant Id: " + aksTenantId);
-  /* End of AKS Subscription Informations */
+    console.log("");
+    /* AKS Subscription Informations */
+    console.log("AKS Azure Subscription Id: " + aksSubcriptionId);
+    console.log("AKS ServicePrincipalId: " + aksServicePrincipalId);
+    console.log("AKS ServicePrincipalKey: " + aksServicePrincipalKey);
+    console.log("AKS Tenant Id: " + aksTenantId);
+    /* End of AKS Subscription Informations */
 
-  console.log("");
-  console.log("Register mode: " + registerMode);
-  console.log("ACR Resource Group: '" + acrResourceGroup + "'");
-  console.log("Container Registry: " + containerRegistry);
-  console.log("AKS Resource Group: " + aksResourceGroup);
-  console.log("AKS Cluster: " + aksCluster);
+    console.log("");
+    console.log("Register mode: " + registerMode);
+    console.log("ACR Resource Group: '" + acrResourceGroup + "'");
+    console.log("Container Registry: " + containerRegistry);
+    console.log("AKS Resource Group: " + aksResourceGroup);
+    console.log("AKS Cluster: " + aksCluster);
 
-  if(acrUsername != undefined){
-      console.log("ACR Username: " + acrUsername);
-      console.log("ACR Password: " + acrPassword);
-  }
-
-  const aksCreds:any = await LoginToAzure(aksServicePrincipalId, aksServicePrincipalKey, aksTenantId);
-  if(registerMode === "aksSecret") {
-    console.log("AKS Secret Access mode");
-    throw new Error("AKS Secret access mode not implemented yet");
-    /*
-        Old Powershell code algo
-        $acrInfo = az acr show --name $containerRegistry -g $acrResourceGroup --subscription $acrSubscriptionId | ConvertFrom-Json
-        if(-not $acrInfo.adminUserEnabled){
-            throw "Container registry named '$containerRegistry' does not have adminUser configured"
-        }
-    */
-  } else {
-    console.log("RBAC Access mode");
-    console.log("Looking for Azure Kubernetes service cluster ...");
-
-    let aksResourceClient = new resourceManagement.ResourceManagementClient(aksCreds, aksSubcriptionId);
-    let rsList = await aksResourceClient.resources.list();
-    let aksClusterInstance:any = rsList.find((element: any) => {
-      return element.name === aksCluster;
-    });
-
-    let aksInfoResult = await aksResourceClient.resources.getById(aksClusterInstance?.id, '2019-10-01');
-    const clientId = aksInfoResult.properties.servicePrincipalProfile.clientId;
-    let aksAppCreds:any = new msRestNodeAuth.ApplicationTokenCredentials(aksCreds.clientId, aksTenantId, aksCreds.secret, 'graph');
-    let aksGraphClient = new graph.GraphRbacManagementClient(aksAppCreds, aksTenantId, { baseUri: 'https://graph.windows.net' });
-    let aksFilterValue = "appId eq '" + clientId + "'";
-    let aksServiceFilter = { filter: aksFilterValue };
-
-    let aksSearch = await aksGraphClient.servicePrincipals.list(aksServiceFilter);
-    let aksServicePrincipal:any = aksSearch.find((element : any) => {
-      return element.appId === clientId;
-    });
-
-    if(aksServicePrincipal === undefined){
-      throw new Error("AKS Service Principal not found");
+    if(acrUsername != undefined){
+        console.log("ACR Username: " + acrUsername);
+        console.log("ACR Password: " + acrPassword);
     }
 
-    // Get the Azure Container Registry Resource infos
-    let acrCreds:any = await LoginToAzure(acrServicePrincipalId, acrServicePrincipalKey, acrTenantId);
-    let acrResourceClient = new resourceManagement.ResourceManagementClient(acrCreds, acrSubcriptionId);
-    let acrResult = await acrResourceClient.resources.list();
-    let acrInstance:any = acrResult.find((element:any)=> {
-      return element.name === containerRegistry;
-    });
-    
-    if(acrInstance === undefined){
-      throw new Error("Azure Container Registry instance not found");
-    }
-
-    let acrAuthClient = new auth.AuthorizationManagementClient(acrCreds, acrSubcriptionId);
-    const acrPullRoleName = "AcrPull";
-    let roles = await acrAuthClient.roleDefinitions.list("/");
-    let acrRole:any = roles.find((role:any)=> {
-      return role.roleName === acrPullRoleName;
-    });
-
-    if(acrRole === undefined){
-      throw new Error("AcrPull not found");
-    }
-
-    let rs = await acrAuthClient.roleAssignments.listForResourceGroup(acrResourceGroup);
-    let roleAssignment = rs.find((elm:any)=> {
-      const rolId = "/subscriptions/" + acrSubcriptionId + acrRole.id;
-      return rolId === elm.roleDefinitionId && elm.principalId === aksServicePrincipal?.objectId;
-    });
-
-    if(roleAssignment === undefined){
-      var newRoleParm = {
-        roleDefinitionId: acrRole.id,
-        principalId: aksServicePrincipal.objectId
-      };
-
-      console.log(newRoleParm);
-
-      let newRoleResult = await acrAuthClient.roleAssignments.create(acrInstance.id, aksServicePrincipal.objectId, newRoleParm);
-      if(newRoleResult != undefined){
-        console.log("New role assignement created!");
-      }
+    const aksCreds:any = await LoginToAzure(aksServicePrincipalId, aksServicePrincipalKey, aksTenantId);
+    if(registerMode === "aksSecret") {
+      console.log("AKS Secret Access mode");
+      throw new Error("AKS Secret access mode not implemented yet");
+      /*
+          Old Powershell code algo
+          $acrInfo = az acr show --name $containerRegistry -g $acrResourceGroup --subscription $acrSubscriptionId | ConvertFrom-Json
+          if(-not $acrInfo.adminUserEnabled){
+              throw "Container registry named '$containerRegistry' does not have adminUser configured"
+          }
+      */
     } else {
-      console.log("Role assignement already existing");
+      console.log("RBAC Access mode");
+      console.log("Looking for Azure Kubernetes service cluster ...");
+
+      let aksResourceClient = new resourceManagement.ResourceManagementClient(aksCreds, aksSubcriptionId);
+      let rsList = await aksResourceClient.resources.list();
+      let aksClusterInstance:any = rsList.find((element: any) => {
+        return element.name === aksCluster;
+      });
+
+      let aksInfoResult = await aksResourceClient.resources.getById(aksClusterInstance?.id, '2019-10-01');
+      const clientId = aksInfoResult.properties.servicePrincipalProfile.clientId;
+      let aksAppCreds:any = new msRestNodeAuth.ApplicationTokenCredentials(aksCreds.clientId, aksTenantId, aksCreds.secret, 'graph');
+      let aksGraphClient = new graph.GraphRbacManagementClient(aksAppCreds, aksTenantId, { baseUri: 'https://graph.windows.net' });
+      let aksFilterValue = "appId eq '" + clientId + "'";
+      let aksServiceFilter = { filter: aksFilterValue };
+
+      let aksSearch = await aksGraphClient.servicePrincipals.list(aksServiceFilter);
+      let aksServicePrincipal:any = aksSearch.find((element : any) => {
+        return element.appId === clientId;
+      });
+
+      if(aksServicePrincipal === undefined){
+        throw new Error("AKS Service Principal not found");
+      }
+
+      // Get the Azure Container Registry Resource infos
+      let acrCreds:any = await LoginToAzure(acrServicePrincipalId, acrServicePrincipalKey, acrTenantId);
+      let acrResourceClient = new resourceManagement.ResourceManagementClient(acrCreds, acrSubcriptionId);
+      let acrResult = await acrResourceClient.resources.list();
+      let acrInstance:any = acrResult.find((element:any)=> {
+        return element.name === containerRegistry;
+      });
+      
+      if(acrInstance === undefined){
+        throw new Error("Azure Container Registry instance not found");
+      }
+
+      let acrAuthClient = new auth.AuthorizationManagementClient(acrCreds, acrSubcriptionId);
+      const acrPullRoleName = "AcrPull";
+      let roles = await acrAuthClient.roleDefinitions.list("/");
+      let acrRole:any = roles.find((role:any)=> {
+        return role.roleName === acrPullRoleName;
+      });
+
+      if(acrRole === undefined){
+        throw new Error("AcrPull not found");
+      }
+
+      let rs = await acrAuthClient.roleAssignments.listForResourceGroup(acrResourceGroup);
+      let roleAssignment = rs.find((elm:any)=> {
+        const rolId = "/subscriptions/" + acrSubcriptionId + acrRole.id;
+        return rolId === elm.roleDefinitionId && elm.principalId === aksServicePrincipal?.objectId;
+      });
+
+      if(roleAssignment === undefined){
+        var newRoleParm = {
+          roleDefinitionId: acrRole.id,
+          principalId: aksServicePrincipal.objectId
+        };
+
+        console.log(newRoleParm);
+
+        let newRoleResult = await acrAuthClient.roleAssignments.create(acrInstance.id, aksServicePrincipal.objectId, newRoleParm);
+        if(newRoleResult != undefined){
+          console.log("New role assignement created!");
+        }
+      } else {
+        console.log("Role assignement already existing");
+      }
     }
+  } catch (err) {
+    tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
   }
 }
 
